0b2b59002753eddc15e2414309775d9a2348400b
anqiwa <andywang19991118@gmail.com>
2022-11-15 01:20:15 -0500

    created a print statement for ch1_exe1.java

--- src/mySolution/Ch1IntroToObj/exe1/Exe1.java
+++ src/mySolution/Ch1IntroToObj/exe1/Exe1.java
@@ -1,6 +1,7 @@
 package mySolution.Ch1IntroToObj.exe1;
 
 public class Exe1 {
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
+        System.out.println("this is a placeholder for exe 1");
     }
 }
